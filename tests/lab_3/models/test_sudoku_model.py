4ee46d6cbec7b89312c2d0b508d1e6342cc118e1
Fr1kky <tso4343@gmail.com>
2021-12-01 22:38:48 +0200

    Added init sudoku test

--- tests/lab_3/models/test_sudoku_model.py
+++ tests/lab_3/models/test_sudoku_model.py
@@ -1,2 +1,25 @@
 import pytest
 import numpy as np
+from lab_3.models.sudoku import Sudoku
+
+@pytest.mark.parametrize("field",  [[[1,0,2,3],
+                                    [2,3,0,2],
+                                    [3,2,3,1]],
+
+                                    [[2,0],
+                                     [0,0]],
+
+                                    [[6,6,6,6],
+                                    [0,0,0,0],
+                                    [10,1,2,10],
+                                    [5,2,6,7,]],
+
+                                    [[-2,-3,5,0],
+                                    [-2,-6,-7,0],
+                                    [-1,-2,0,0],
+                                    [0,0,-1,2]]])
+def test_init_false(field):
+    with pytest.raises(AssertionError):
+        model = Sudoku(field)
+
+
