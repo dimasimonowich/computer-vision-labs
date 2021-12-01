e8e386f7f438a3d758b54b8c59d1969a35ad8f7f
Your Namedimasimonowich <dima.simonowich@gmail.com>
2021-12-01 20:59:32 +0200

    Added separated field labels init func

--- lab_3/models/sudoku.py
+++ lab_3/models/sudoku.py
@@ -12,7 +12,8 @@ class Sudoku:
         assert np.all(self.field_array <= self.n)
         # TODO: assert all elements are ints
 
-        self.field_labels = self._get_initial_field_labels()
+        self.field_labels = None
+        self.dependencies = None
 
     def _get_initial_field_labels(self):
         field_labels = []
@@ -22,6 +23,7 @@ class Sudoku:
 
             for col_id in range(self.n):
                 field_value = self.field_array[row_id][col_id]
+                field_label = []
 
                 if field_value == 0:
                     field_label = list(range(1, self.n + 1))
@@ -33,4 +35,45 @@ class Sudoku:
 
         return np.array(field_labels)
 
+    def _get_arcs(self):
+
+        def get_row_arcs(field_labels, row_id):
+            row_labels = field_labels[row_id]
+            print(row_labels)
+            return
+
+        def get_column_arcs(field_labels, col_id):
+            col_labels = field_labels[:, col_id]
+            print(col_labels)
+            return
+
+        def get_block_arcs(field_labels, block_row_block, block_col_block, n):
+            block_labels = field_labels[block_row_block * n: block_row_block * (n + 1), block_col_block * n: block_col_block * (n + 1),]
+            print(block_labels)
+            return
+
+        def get_block_id(row_id, col_id, n):
+            block_col_block = col_id // n
+            block_row_block = row_id // n
+
+            return block_row_block, block_col_block
+
+        arcs = np.zeros_like(self.field_array)
+
+        for row_id in range(self.n):
+            for col_id in range(self.n):
+                block_row_block, block_col_block = get_block_id(row_id, col_id, self.n)
+
+                row_arcs = get_row_arcs(self.field_labels, row_id)
+                column_arcs = get_column_arcs(self.field_labels, col_id)
+                block_arcs = get_block_arcs(self.field_labels, block_row_block, block_col_block)
+
+                running_arcs = row_arcs + column_arcs + block_arcs
+                arcs[row_id][col_id] = running_arcs
+
+        return arcs
+
+    def prepare_to_ac(self):
+        self.field_labels = self._get_initial_field_labels()
+        self.arcs = self._get_arcs()
 
