75ee207700cc64cbeb65a07bca01a9642bdbb8e2
Your Namedimasimonowich <dima.simonowich@gmail.com>
2021-12-01 20:21:18 +0200

    Added initial labels filling

--- lab_3/models/sudoku.py
+++ lab_3/models/sudoku.py
@@ -1,56 +1,36 @@
 import numpy as np
 
 
-class SudokuBox:
-    def __init__(self, row_id, col_id, field):
-        self.row_id = row_id
-        self.col_id = col_id
-
-        self.labels = self._get_labels(field)
-        self.related = self._get_related(field)
-
-    def _get_labels(self, field):
-        relative_row = field[self.row_id][self.col_id]
-
-        return np.array(related)
-
-
-    def _get_related(self, field):
-        relative_row = field[self.row_id]
-        relative_col = field[:, self.col_id]
-        relative_block
-        related = set(relative_row + relative_col + relative_block)
-
-        return np.array(related)
-
-
 class Sudoku:
-    def __init__(self, field_array):
-        assert field_array.shape[0] == field_array.shape[1]
-        assert field_array.shape[0] >= 4
-        assert field_array.shape[0] % 2 == 0
+    def __init__(self, field):
+        self.field_array = np.array(field)
+        self.n = self.field_array.shape[0]
 
-        self.field_array = field_array
-        self.n = field_array.shape[0]
+        assert self.field_array.shape[0] == self.field_array.shape[1]
+        assert self.n >= 4
+        assert np.all(self.field_array >= 0)
+        assert np.all(self.field_array <= self.n)
+        # TODO: assert all elements are ints
 
-        self.field = None
+        self.field_labels = self._get_initial_field_labels()
 
-    @staticmethod
-    def _create_field(field):
-        field = []
+    def _get_initial_field_labels(self):
+        field_labels = []
 
         for row_id in range(self.n):
+            row_labels = []
+
             for col_id in range(self.n):
-                sudoku_box = SudokuBox(row_id, col_id, field)
+                field_value = self.field_array[row_id][col_id]
 
+                if field_value == 0:
+                    field_label = list(range(1, self.n + 1))
+                elif field_value > 0:
+                    field_label = [field_value]
+                row_labels.append(field_label)
 
+            field_labels.append(row_labels)
 
+        return np.array(field_labels)
 
-
-    # def get_relatives(self, row_id, col_id):
-    #     relative_row = self.field[row_id]
-    #     relative_col = self.field[:, col_id]
-    #     relative_block =
-    #
-    #     relatives =
 
