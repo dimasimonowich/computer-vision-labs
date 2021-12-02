c231dc4e36a6a6b551912ff19a4df47af8b7862e
dimasimonowich <dima.simonowich@gmail.com>
2021-12-02 07:14:01 +0200

    Updated fields filling structure

--- lab_3/models/sudoku.py
+++ lab_3/models/sudoku.py
@@ -12,16 +12,24 @@ class Sudoku:
         assert np.all(self.field_array <= self.n)
         # TODO: assert all elements are ints
 
-        self.field_labels = None
+        self.fields = None
         self.dependencies = None
 
-    def _get_initial_field_labels(self):
-        field_labels = []
+    def _get_initial_fields_filling(self):
+        def get_block_ids(row_id, col_id, n):
+            block_col_id = col_id // (n**0.5)
+            block_row_id = row_id // (n**0.5)
+
+            return int(block_row_id), int(block_col_id)
+
+        fields = []
 
         for row_id in range(self.n):
-            row_labels = []
+            row_fields = []
 
             for col_id in range(self.n):
+                field = {}
+
                 field_value = self.field_array[row_id][col_id]
                 field_label = []
 
@@ -29,44 +37,57 @@ class Sudoku:
                     field_label = list(range(1, self.n + 1))
                 elif field_value > 0:
                     field_label = [field_value]
-                row_labels.append(field_label)
 
-            field_labels.append(row_labels)
+                field['row_id'] = row_id
+                field['col_id'] = col_id
 
-        return np.array(field_labels)
+                block_row_id, block_col_id = get_block_ids(row_id, col_id, self.n)
+                field['block_row_id'] = block_row_id
+                field['block_col_id'] = block_col_id
+
+                field['labels'] = field_label
+
+                row_fields.append(field)
+
+            fields.append(row_fields)
+
+        return fields
 
     def _get_arcs(self):
 
         def get_row_arcs(field_labels, row_id):
             row_labels = field_labels[row_id]
-            print(row_labels)
-            return
+            row_arcs = {}
+
+            for i, labels in enumerate(row_labels):
+                for j, label in enumerate(labels):
+
+                    row_arcs[f"{i}, {j}"] = tuple(label, )
+
+
+            return row_labels
 
         def get_column_arcs(field_labels, col_id):
             col_labels = field_labels[:, col_id]
-            print(col_labels)
-            return
+            return col_labels
 
-        def get_block_arcs(field_labels, block_row_block, block_col_block, n):
-            block_labels = field_labels[block_row_block * n: block_row_block * (n + 1), block_col_block * n: block_col_block * (n + 1),]
-            print(block_labels)
-            return
+        def get_block_arcs(field_labels, block_row_id, block_col_id, n):
+            print(block_row_id * n, (block_row_id + 1) * n)
+            block_labels = field_labels[block_row_id * n: (block_row_id + 1) * n,
+                           block_col_id * n: (block_col_id + 1) * n]
+            return block_labels
 
-        def get_block_id(row_id, col_id, n):
-            block_col_block = col_id // n
-            block_row_block = row_id // n
 
-            return block_row_block, block_col_block
 
         arcs = np.zeros_like(self.field_array)
 
         for row_id in range(self.n):
             for col_id in range(self.n):
-                block_row_block, block_col_block = get_block_id(row_id, col_id, self.n)
+                block_row_id, block_col_id = get_block_ids(row_id, col_id, self.n)
 
                 row_arcs = get_row_arcs(self.field_labels, row_id)
                 column_arcs = get_column_arcs(self.field_labels, col_id)
-                block_arcs = get_block_arcs(self.field_labels, block_row_block, block_col_block)
+                block_arcs = get_block_arcs(self.field_labels, block_row_id, block_col_id)
 
                 running_arcs = row_arcs + column_arcs + block_arcs
                 arcs[row_id][col_id] = running_arcs
