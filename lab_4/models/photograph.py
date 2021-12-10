c24937c87cb01792864076f460940e781a8f6ea8
Your Namedimasimonowich <dima.simonowich@gmail.com>
2021-12-10 23:13:34 +0200

    Added class init func

---
+++ lab_4/models/photograph.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+class PhotoGraph:
+    def __init__(self, images, masks, alpha=1, beta=1):
+        assert type(images) == np.ndarray
+        assert type(masks) == np.ndarray
+        assert images.shape == masks.shape
+        assert alpha >= 0
+        assert beta >= 0
+
+        self.K = len(images)
+        self.images = images
+        self.masks = masks
+        self.alpha = alpha
+        self.images_pixels_costs = None
+        self.transitions_costs = None
+
+
