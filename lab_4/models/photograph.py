d3e98c470419e8a6dea41933bde4139b16d77a72
Your Namedimasimonowich <dima.simonowich@gmail.com>
2021-12-10 23:59:45 +0200

    Added get_transitions_costs func

--- lab_4/models/photograph.py
+++ lab_4/models/photograph.py
@@ -9,7 +9,9 @@ class PhotoGraph:
         assert alpha >= 0
         assert beta >= 0
 
-        self.K = len(images)
+        self.high = images.shape[1]
+        self.width = images.shape[2]
+        self.num_images = len(images)
         self.images = images
         self.masks = masks
         self.alpha = alpha
@@ -19,4 +21,23 @@ class PhotoGraph:
     def get_pixels_costs(self):
         return self.alpha * (1 - self.masks)
 
+    def get_transitions_costs(self):
+        def shift_right(images, shift):
+            shifted_images = np.zeros_like(images)
+            shifted_images[:, :, shift:] = images[:, :, shift:]
+
+            return shifted_images
+
+        shifted_images = shift_right(self.images, 1)
+
+        # transitions_costs = np.zeros((row, from_pixel, from_image, to_image)
+        transitions_costs = np.zeros((self.high, self.width, self.num_images, self.num_images))
+
+        for from_image_id in range(self.num_images):
+            for to_image_id in range(self.num_images):
+                transitions_costs[:, :, from_image_id, to_image_id] = \
+                    beta * (np.linalg.norm((images[from_image_id] - images[to_image_id]), ord=1, axis=2) +
+                            np.linalg.norm((shifted_images[from_image_id] - shifted_images[to_image_id]), ord=1, axis=2))
+
+        return transitions_costs
 
