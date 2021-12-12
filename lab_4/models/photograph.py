7231bc8187b7c038b61b79a2d0e2f24c38b3192a
Your Namedimasimonowich <dima.simonowich@gmail.com>
2021-12-12 17:29:40 +0200

    Added get_total_costs func

--- lab_4/models/photograph.py
+++ lab_4/models/photograph.py
@@ -5,7 +5,7 @@ class PhotoGraph:
     def __init__(self, images, masks, alpha=1, beta=1):
         assert type(images) == np.ndarray
         assert type(masks) == np.ndarray
-        assert images.shape == masks.shape
+        assert images[:, :, :, 0].shape == masks.shape
         assert alpha >= 0
         assert beta >= 0
 
@@ -16,8 +16,8 @@ class PhotoGraph:
         self.masks = masks
         self.alpha = alpha
         self.beta = beta
-        self.images_pixels_costs = None
-        self.transitions_costs = None
+        self.pixels_costs = self.get_pixels_costs()
+        self.transitions_costs = self.get_transitions_costs()
 
     def get_pixels_costs(self):
         return self.alpha * (1 - self.masks)
@@ -32,16 +32,29 @@ class PhotoGraph:
         shifted_images = shift_right(self.images, 1)
 
         # transitions_costs = np.zeros((row, from_pixel, from_image, to_image)
-        transitions_costs = np.zeros((self.high, self.width, self.num_images, self.num_images))
+        transitions_costs = np.zeros((self.num_images, self.num_images, self.high, self.width))
 
         for from_image_id in range(self.num_images):
             for to_image_id in range(self.num_images):
-                transitions_costs[:, :, from_image_id, to_image_id] = \
+                transitions_costs[from_image_id, to_image_id, :, :] = \
                     self.beta * (np.linalg.norm((self.images[from_image_id] - self.images[to_image_id]), ord=1, axis=2) +
-                            np.linalg.norm((shifted_images[from_image_id] - shifted_images[to_image_id]), ord=1, axis=2))
+                                 np.linalg.norm((shifted_images[from_image_id] - shifted_images[to_image_id]), ord=1, axis=2))
 
         return transitions_costs
 
+    def get_total_costs(self):
+        total_costs = np.zeros((self.num_images, self.high, self.width))
 
+        for n in range(self.high - 1, 0, -1):
+            pixels_col_cost = self.pixels_costs[:, :, n]
+            transitions_col_costs = self.transitions_costs[:, :, :, n]
+
+            if n == self.high - 1:
+                total_costs[:, :, n] = np.min(pixels_col_cost + transitions_col_costs, axis=1)
+            else:
+                total_costs[:, :, n] = np.min(pixels_col_cost + transitions_col_costs +
+                                              total_costs[:, :, n + 1], axis=1)
+
+        return total_costs
 
 
