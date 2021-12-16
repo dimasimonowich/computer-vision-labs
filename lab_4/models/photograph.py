0f7b109be329a449708aeb06ab8b0059b1eebfaa
Your Namedimasimonowich <dima.simonowich@gmail.com>
2021-12-16 18:29:57 +0200

    Fixed transition cost function

--- lab_4/models/photograph.py
+++ lab_4/models/photograph.py
@@ -40,7 +40,7 @@ class PhotoGraph:
         for from_image_id in range(self.num_images):
             for to_image_id in range(self.num_images):
                 transitions_costs[from_image_id, to_image_id, :, :] = \
-                    self.beta * (np.linalg.norm((self.images[from_image_id] - self.images[to_image_id]), ord=1, axis=2) *
+                    self.beta * (np.linalg.norm((self.images[from_image_id] - self.images[to_image_id]), ord=1, axis=2) +
                                  np.linalg.norm((shifted_images[from_image_id] - shifted_images[to_image_id]), ord=1, axis=2))
 
         return transitions_costs
