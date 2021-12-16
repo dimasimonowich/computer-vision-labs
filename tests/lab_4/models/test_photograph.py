e629065120b91f531677d0dfc0af1954a845eb52
Fr1kky <tso4343@gmail.com>
2021-12-16 18:58:35 +0200

    Created initial tests

---
+++ tests/lab_4/models/test_photograph.py
@@ -0,0 +1,14 @@
+import pytest
+import numpy as np
+from lab_4.models.photograph import PhotoGraph
+
+@pytest.mark.parametrize('image, mask, alpha, beta', [([[[123, 123, 123],[221, 132, 221],[123, 221, 221]],
+                                                         [[12,32,34], [54,65,32], [12,36,78]],
+                                                         [[56,62,52], [1,1,1], [0,0,0]]],
+                                                        [[0,0,255],
+                                                         [0,255,0],
+                                                         [0,0,255]], 1, 1)])
+def test_init_true(image, mask, alpha, beta):
+    with pytest.raises(AssertionError):
+        model = PhotoGraph(image, mask, alpha, beta)
+
