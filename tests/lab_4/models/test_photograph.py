353647c55d072fe1751ea7f8dfe96d94c262f039
Fr1kky <tso4343@gmail.com>
2021-12-16 19:30:23 +0200

    Added test for photograph merge images func

--- tests/lab_4/models/test_photograph.py
+++ tests/lab_4/models/test_photograph.py
@@ -111,4 +111,32 @@ def  test_get_mask_idxes_true(images, masks, alpha, beta, res):
     to_compare = model._get_mask_idxes()
     np.testing.assert_array_almost_equal(to_compare, res, decimal=1)
 
+@pytest.mark.parametrize('images, masks, alpha, beta, res', [(np.array([[[[122, 123, 123],[221, 132, 221],[123, 221, 221]],
+                                                                     [[11,32,34], [54,65,32], [12,36,78]],
+                                                                     [[55,62,52], [1,1,1], [0,0,0]]],
+                                                                     [[[123, 224, 13], [221, 212, 21], [113, 221, 121]],
+                                                                     [[190, 183, 72], [255, 212, 93], [111, 61, 78]],
+                                                                     [[8, 27, 52], [41, 222, 220], [200, 100, 0]]]]),
+                                                        np.array([[[0,0,255],
+                                                                  [0,255,0],
+                                                                  [0,0,255]],
+                                                                  [[0,0,0],
+                                                                   [255,0,0],
+                                                                   [255,0,0]]]), 1, 1,
+                                                              np.array([[[0.48235294, 0.48235294, 0.48235294],
+                                                                         [0.86666667, 0.51764706, 0.86666667],
+                                                                         [0.48235294, 0.86666667, 0.86666667]],
+
+                                                                         [[0.74509804, 0.71764706, 0.28235294],
+                                                                         [1.        , 0.83137255, 0.36470588],
+                                                                         [0.43529412, 0.23921569, 0.30588235]],
+
+                                                                         [[0.03137255, 0.10588235, 0.20392157],
+                                                                         [0.16078431, 0.87058824, 0.8627451 ],
+                                                                         [0.78431373, 0.39215686, 0.        ]]]))])
+def test_merge_images_true(images, masks, alpha, beta, res):
+    model = PhotoGraph(images / 255, masks / 255, alpha, beta)
+    to_compare = model.merge_images()
+    np.testing.assert_array_almost_equal(to_compare, res, decimal=1)
+
 
