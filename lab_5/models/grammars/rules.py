ea045e6f7ff06654352fe899f32205632b36f448
Your Namedimasimonowich <dima.simonowich@gmail.com>
2021-12-26 05:54:42 +0200

    Added initial rules

---
+++ lab_5/models/grammars/rules.py
@@ -0,0 +1,11 @@
+def vconcat(terms, result):
+    upper = terms[0]
+    lower = terms[1]
+
+    return upper, lower, result
+
+def hconcat(terms, result):
+    right = terms[0]
+    left = terms[1]
+
+    return right, left, result
