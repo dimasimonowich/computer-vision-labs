d637b2c4f4f04fa2758eb659d435f4c567293b91
Your Namedimasimonowich <dima.simonowich@gmail.com>
2021-12-27 02:26:06 +0200

    Added result check and print functions, updated predict

--- lab_5/models/bin_sum_check.py
+++ lab_5/models/bin_sum_check.py
@@ -1,12 +1,15 @@
 from .grammars_core import Grammars
 from .rules import hconcat, vconcat
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 class BinSumCheck(Grammars):
-    def __init__(self, train_data, rules, objects, max_iter=100):
+    def __init__(self, train_data, rules, objects, final_symbol, transition_symbol, early_stoping_symbol=None):
         super().__init__(train_data, rules, objects)
-        self.max_iter = max_iter
+        self.final_symbol = final_symbol
+        self.early_stoping_symbol = early_stoping_symbol
+        self.transition_symbol = transition_symbol
 
     def kek_predict(self, image):
         initial_markup = self.get_initial_markup(image)
@@ -48,27 +51,86 @@ class BinSumCheck(Grammars):
 
         return vconcat_rules, hconcat_rules, np.unique(result_shapes, axis=0)
 
+    def _check_result(self, markup):
+        resulting_markup = np.ones(markup.shape, dtype=int) * (-1)
+        requires_transition = False
+
+        for row_id, labels_row in enumerate(markup):
+            for col_id, label in enumerate(labels_row):
+
+                if self.final_symbol in label:
+                    resulting_markup[row_id][col_id] = 1
+
+                if self.early_stoping_symbol:
+                    if self.early_stoping_symbol in label:
+                        resulting_markup[row_id][col_id] = 0
+
+                if col_id == 1 and self.transition_symbol in label:
+                    requires_transition = True
+
+        return resulting_markup[:, 1:-1], requires_transition
+
     def predict(self, image):
         def get_image_views(image, shapes):
             return np.lib.stride_tricks.sliding_window_view(image, shapes)
 
+        def add_borders(markup):
+            empty_col = np.empty((markup.shape[0], 1), dtype=object)
+
+            for i in range(markup.shape[0]):
+                empty_col[i, 0] = list(["-1"])
+
+            return np.hstack([empty_col, markup, empty_col])
+
         markup = self.get_initial_markup(image)
+        markup = add_borders(markup)
+
         vconcat_rules, hconcat_rules, unique_shapes = self._parse_rules()
 
         for shapes in unique_shapes:
-            if -1 not in shapes:
-                views = get_image_views(markup, shapes)
-                vconcat_rules_with_shapes = []
-                hconcat_rules_with_shapes = []
+            views = get_image_views(markup, shapes)
+            vconcat_rules_with_shapes = []
+            hconcat_rules_with_shapes = []
 
-                for vconcat_rule in vconcat_rules:
-                    if np.all(vconcat_rule.get("result_shape") == shapes):
-                        vconcat_rules_with_shapes.append(vconcat_rule)
+            for vconcat_rule in vconcat_rules:
+                if np.all(vconcat_rule.get("result_shape") == shapes):
+                    vconcat_rules_with_shapes.append(vconcat_rule)
 
-                for hconcat_rule in hconcat_rules:
-                    if np.all(hconcat_rule.get("result_shape") == shapes):
-                        hconcat_rules_with_shapes.append(hconcat_rule)
+            for hconcat_rule in hconcat_rules:
+                if np.all(hconcat_rule.get("result_shape") == shapes):
+                    hconcat_rules_with_shapes.append(hconcat_rule)
 
-                markup = vconcat(markup, vconcat_rules_with_shapes, views)
-                markup = hconcat(markup, hconcat_rules_with_shapes, views)
+            markup = vconcat(markup, vconcat_rules_with_shapes, views)
+            markup = hconcat(markup, hconcat_rules_with_shapes, views)
+
+        resulting_markup, requires_transition = self._check_result(markup)
+
+        return resulting_markup, requires_transition, markup
+
+    @staticmethod
+    def draw_results(image, resulting_markup, requires_transition):
+        addition_correct = True
+        colored_image = np.stack([image, image, image], axis=2)
+
+        if np.any(resulting_markup == 0):
+            addition_correct = False
+
+            shapes_ratio = image.shape[1] / resulting_markup.shape[1]
+            for idx, element in enumerate(resulting_markup[0]):
+                if element == 0:
+                    colored_image[:, int(idx * shapes_ratio):int((idx + 1) * shapes_ratio), 0] = \
+                        np.where(colored_image[:, int(idx * shapes_ratio):int((idx + 1) * shapes_ratio), 0] == 0, 1,
+                                 colored_image[:, int(idx * shapes_ratio):int((idx + 1) * shapes_ratio), 0])
+
+        plt.imshow(colored_image)
+        plt.show()
+
+        if addition_correct:
+            print("Addition is correct!")
+            if requires_transition:
+                print("Transition required!")
+            else:
+                print("No transition!")
+        else:
+            print("Addition is incorrect!")
 
