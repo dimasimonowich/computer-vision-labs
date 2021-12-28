d4b649302d95509a990ffa8411d26b19d27da510
Your Namedimasimonowich <dima.simonowich@gmail.com>
2021-12-28 23:17:32 +0200

    Updated image classification

--- lab_5/models/bin_sum_check.py
+++ lab_5/models/bin_sum_check.py
@@ -5,22 +5,11 @@ from matplotlib import pyplot as plt
 
 
 class BinSumCheck(Grammars):
-    def __init__(self, train_data, rules, objects, final_symbol, transition_symbol, early_stoping_symbol=None):
+    def __init__(self, train_data, rules, objects, final_symbol, transition_symbol, early_stopping_symbol=None):
         super().__init__(train_data, rules, objects)
         self.final_symbol = final_symbol
-        self.early_stoping_symbol = early_stoping_symbol
+        self.early_stoping_symbol = early_stopping_symbol
         self.transition_symbol = transition_symbol
-
-    def kek_predict(self, image):
-        initial_markup = self.get_initial_markup(image)
-
-        term_0 = int(f"{int(initial_markup[0][0])}{int(initial_markup[0][1])}{int(initial_markup[0][2])}", base=2)
-        term_1 = int(f"{int(initial_markup[1][0])}{int(initial_markup[1][1])}{int(initial_markup[1][2])}", base=2)
-        result = int(f"{int(initial_markup[2][0])}{int(initial_markup[2][1])}{int(initial_markup[2][2])}", base=2)
-
-        int_sum = term_0 + term_1
-
-        return result == int_sum
 
     def _parse_rules(self):
         vconcat_rules = []
--- lab_5/models/grammars_core.py
+++ lab_5/models/grammars_core.py
@@ -1,5 +1,4 @@
 import numpy as np
-from sklearn.metrics import mean_squared_error
 from matplotlib import pyplot as plt
 
 
@@ -9,7 +8,7 @@ class Grammars:
         self.rules = rules
         self.objects = objects
 
-    def _classify(self, image, threshold=0.1):
+    def _classify(self, image):
         predicted_class = None
 
         for data_tuple in self.train_data:
@@ -19,14 +18,13 @@ class Grammars:
             for train_image in train_images:
                 image_diag = np.diagonal(image)
                 train_image_diag = np.diagonal(train_image)
-                mse = mean_squared_error(image_diag, train_image_diag)
 
-                if mse < threshold:
+                if np.all(image_diag == train_image_diag):
                     predicted_class = label
-                    return predicted_class, mse
+                    return predicted_class
 
         print("ERROR: Image can not be classified!")
-        return None, None
+        return predicted_class
 
     def _get_atoms(self, image):
         def split_image(image, chunk_shape):
@@ -60,7 +58,7 @@ class Grammars:
 
         for row_id, atom_row in enumerate(atoms):
             for col_id, atom in enumerate(atom_row):
-                atom_class, _ = self._classify(atom)
+                atom_class = self._classify(atom)
 
                 if atom_class:
                     initial_markup[row_id][col_id] = list(str(atom_class))
